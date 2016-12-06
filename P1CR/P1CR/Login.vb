35f739511bd7cae78eb251e0fc53ac202bf28ace
Carlitos lol <chobertboricua@hotmail.com>
2016-12-06 14:12:11 -0400

    commit2

--- P1CR/P1CR/Login.vb
+++ P1CR/P1CR/Login.vb
@@ -3,13 +3,6 @@
     Dim p As Integer = 0
     Dim sqlStr2 As String
 
-    ' TODO: Insert code to perform custom authentication using the provided username and password 
-    ' (See http://go.microsoft.com/fwlink/?LinkId=35339).  
-    ' The custom principal can then be attached to the current thread's principal as follows: 
-    '     My.User.CurrentPrincipal = CustomPrincipal
-    ' where CustomPrincipal is the IPrincipal implementation used to perform authentication. 
-    ' Subsequently, My.User will return identity information encapsulated in the CustomPrincipal object
-    ' such as the username, display name, etc.
 
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
         Dim table As New DataTable
--- P1CR/P1CR/Module1.vb
+++ P1CR/P1CR/Module1.vb
@@ -2,6 +2,7 @@
 Imports Oracle.ManagedDataAccess.Types
 Module Modulo1
     Public Const oradb = "DATA SOURCE=LAPTOP-ND4K7KG8:1521/xe;PASSWORD=;USER ID=ELCARLOS"
+    Public conn As New Oracle.ManagedDataAccess.Client.OracleConnection(oradb)
     'comment
     'comment
 End Module
