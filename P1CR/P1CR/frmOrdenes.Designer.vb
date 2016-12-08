45ac62d8d0435aa866cd761fd2ebc9df34beb0b5
Owner <me@me.com>
2016-12-08 18:26:19 -0400

    6:26

--- P1CR/P1CR/frmOrdenes.Designer.vb
+++ P1CR/P1CR/frmOrdenes.Designer.vb
@@ -22,8 +22,72 @@ Partial Class frmOrdenes
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.TSLblOrder = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.TStxtOrderNo = New System.Windows.Forms.ToolStripTextBox()
+        Me.dgvOrders = New System.Windows.Forms.DataGridView()
+        Me.ToolStrip1.SuspendLayout()
+        CType(Me.dgvOrders, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSLblOrder, Me.ToolStripSeparator1, Me.TStxtOrderNo})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(284, 25)
+        Me.ToolStrip1.TabIndex = 0
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'TSLblOrder
+        '
+        Me.TSLblOrder.Name = "TSLblOrder"
+        Me.TSLblOrder.Size = New System.Drawing.Size(89, 22)
+        Me.TSLblOrder.Text = "Enter Order No:"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'TStxtOrderNo
+        '
+        Me.TStxtOrderNo.Name = "TStxtOrderNo"
+        Me.TStxtOrderNo.Size = New System.Drawing.Size(100, 25)
+        '
+        'dgvOrders
+        '
+        Me.dgvOrders.AllowUserToAddRows = False
+        Me.dgvOrders.AllowUserToDeleteRows = False
+        Me.dgvOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvOrders.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvOrders.Location = New System.Drawing.Point(0, 25)
+        Me.dgvOrders.Name = "dgvOrders"
+        Me.dgvOrders.ReadOnly = True
+        Me.dgvOrders.Size = New System.Drawing.Size(284, 236)
+        Me.dgvOrders.TabIndex = 1
+        '
+        'frmOrdenes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.dgvOrders)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Name = "frmOrdenes"
         Me.Text = "frmOrdenes"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        CType(Me.dgvOrders, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents TSLblOrder As ToolStripLabel
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents TStxtOrderNo As ToolStripTextBox
+    Friend WithEvents dgvOrders As DataGridView
 End Class
