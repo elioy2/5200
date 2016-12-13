c377c4b3fb7c667464b039c5c865d2216a7bd042
Eloy <Eloy@SHADOW-GHOST>
2016-12-12 23:29:34 -0400

    12354.NU

--- P1CR/P1CR/Module1.vb
+++ P1CR/P1CR/Module1.vb
@@ -1,7 +1,7 @@
 ﻿Imports Oracle.ManagedDataAccess.Client
 Imports Oracle.ManagedDataAccess.Types
 Module Modulo1
-    Public Const oradb = "DATA SOURCE=Shadow-Ghost:1521/xe;PASSWORD=qazplm741;USER ID=system"
+    Public Const oradb = "DATA SOURCE=Shadow-Ghost:1521/xe;PASSWORD=;USER ID=system"
     'Public conn As New Oracle.ManagedDataAccess.Client.OracleConnection(oradb)
     'comment
     'comment
